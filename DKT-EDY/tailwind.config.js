a16a66e40643be0f0d1f7f5dcaa0204fae6160c8
dygozturk <duygu_ozturk20@erdogan.edu.tr>
2025-12-03 17:10:13 +0300

    "first commit"

---
+++ DKT-EDY/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        sage: "#9DB8A6",
+        sand: "#F3EDE5",
+        cream: "#FAF7F3",
+        forest: "#2F4F4F",
+        terracotta: "#D58C6A"
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+}
